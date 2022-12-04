934129b4de7b4ea2147aff5d7ff7586cdf9bbe62
excelforte-github <naras@excelforte.com>
2022-12-04 18:01:48 +0530

    04.12.2022
    15. modifications added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './admin/about/about.component';
-import { DashboardComponent } from './admin/dashboard/dashboard.component';
-import { ProjectsComponent } from './admin/projects/projects.component';
-import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent }  
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
