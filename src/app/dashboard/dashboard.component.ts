f9f0d20713718ce2edd35bb5a2f485c52d166ea1
excelforte-github <naras@excelforte.com>
2022-12-04 21:39:04 +0530

    04.12.2022
    20.create dashboard has completed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number = 0;
+  UpComingProjects: number = 0;
+  ProjectCost: number = 0;
+  CurrentExpenditure: number = 0;
+  AvailableFunds: number = 0;
+
+  Clients: string[] = [];
+  Projects: string[] = [];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
+
+  ngOnInit() {
+    this.Designation = 'Team Leader';
+    this.Username = 'John Smith';
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 0.2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
+  }
 }
+
+
+
+
