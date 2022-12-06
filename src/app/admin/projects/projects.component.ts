951c2e6f447ad3760f7d530dde130dbc0c06b975
kannanwin <kskannan.win@gmail.com>
2022-12-06 17:15:27 +0530

    06.12.2022
    41.delete request in angular has completed

--- src/app/admin/projects/projects.component.ts
+++ src/app/admin/projects/projects.component.ts
@@ -76,4 +76,26 @@ export class ProjectsComponent {
       }
     );
   }
+
+  onDeleteClick(event: any, index: number) {
+    this.deleteIndex = index;
+    this.deleteProject.projectID = this.projects[index].projectID;
+    this.deleteProject.projectName = this.projects[index].projectName;
+    this.deleteProject.dateOfStart = this.projects[index].dateOfStart;
+    this.deleteProject.teamSize = this.projects[index].teamSize;
+  }
+
+  onDeleteConfirmClick() {
+    this.projectsService.deleteProject(this.deleteProject.projectID).subscribe(
+      (response) => {
+        this.projects.splice(this.deleteIndex, 1);
+        this.deleteProject.projectID = null;
+        this.deleteProject.projectName = null;
+        this.deleteProject.teamSize = null;
+        this.deleteProject.dateOfStart = null;
+      },
+      (error) => {
+        console.log(error);
+      });
+  }
 }
--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -13,14 +13,18 @@ export class ProjectsService {
   constructor(private httpClient: HttpClient) { }
 
   getAllProjects(): Observable<Project[]> {
-    return this.httpClient.get<Project[]>(this.urlPrefix + "/api/projects");
+    return this.httpClient.get<Project[]>(this.urlPrefix + "/api/projects", { responseType: "json" });
   }
 
   insertProject(newProject: Project): Observable<Project> {
-    return this.httpClient.post<Project>(this.urlPrefix + "/api/projects", newProject);
+    return this.httpClient.post<Project>(this.urlPrefix + "/api/projects", newProject, { responseType: "json" });
   }
 
   updateProject(existingProject: Project): Observable<Project> {
     return this.httpClient.put<Project>(this.urlPrefix + "/api/projects", existingProject, { responseType: "json" });
+  }
+
+  deleteProject(ProjectID: number): Observable<string> {
+    return this.httpClient.delete<string>(this.urlPrefix + "/api/projects?ProjectID=" + ProjectID);
   }
 }
