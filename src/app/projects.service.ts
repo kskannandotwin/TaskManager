da6a435d44fa205b380aa3715cda0fe815dc1789
kannanwin <kskannan.win@gmail.com>
2022-12-06 17:38:04 +0530

    06.12.2022
    44.rxjs observable-map has completed

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Project } from './project';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,16 @@ export class ProjectsService {
   constructor(private httpClient: HttpClient) { }
 
   getAllProjects(): Observable<Project[]> {
-    return this.httpClient.get<Project[]>(this.urlPrefix + "/api/projects", { responseType: "json" });
+    return this.httpClient
+      .get<Project[]>(this.urlPrefix + '/api/projects', { responseType: 'json' })
+      .pipe(
+        map((data: Project[]) => {
+          for (let i = 0; i < data.length; i++) {
+            data[i].teamSize = data[i].teamSize * 100;
+          }
+          return data;
+        })
+      );
   }
 
   insertProject(newProject: Project): Observable<Project> {
