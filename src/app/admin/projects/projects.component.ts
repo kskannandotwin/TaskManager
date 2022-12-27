04603a84534a85c855b335ba666ff52bf50144e5
kannanwin <kskannan.win@gmail.com>
2022-12-27 14:47:48 +0530

    27.12.2022
    58. validations in template driven forms has completed

--- src/app/admin/projects/projects.component.ts
+++ src/app/admin/projects/projects.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ClientLocation } from 'src/app/client-location';
 import { ClientLocationsService } from 'src/app/client-locations.service';
 import { Project } from 'src/app/project';
 import { ProjectsService } from 'src/app/projects.service';
+import { NgForm } from '@angular/forms';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-projects',
@@ -22,6 +24,9 @@ export class ProjectsComponent implements OnInit {
   searchBy: string = 'ProjectName';
   searchText: string = '';
 
+  @ViewChild('newForm') newForm: NgForm | any = null;
+  @ViewChild('editForm') editForm: NgForm | any = null;
+
   constructor(private projectsService: ProjectsService, private clientLocationsService: ClientLocationsService) {
 
   }
@@ -40,49 +45,15 @@ export class ProjectsComponent implements OnInit {
     );
   }
 
+  onNewClick(event: any) {
+    this.newForm.resetForm();
+  }
+
   onSaveClick() {
-    this.newProject.clientLocation.clientLocationID = 0;
-    this.projectsService.insertProject(this.newProject).subscribe((response) => {
-      //Add Project to Grid
-      var p: Project = new Project();
-      p.projectID = response.projectID;
-      p.projectName = response.projectName;
-      p.dateOfStart = response.dateOfStart;
-      p.teamSize = response.teamSize;
-      p.clientLocation = response.clientLocation;
-      p.active = response.active;
-      p.clientLocationID = response.clientLocationID;
-      p.status = response.status;
-      this.projects.push(p);
-
-      //Clear New Project Dialog - TextBoxes
-      this.newProject.projectID = null;
-      this.newProject.projectName = null;
-      this.newProject.dateOfStart = null;
-      this.newProject.teamSize = null;
-      this.newProject.active = false;
-      this.newProject.clientLocationID = null;
-      this.newProject.status = null;
-    }, (error) => {
-      console.log(error);
-    });
-  }
-
-  onEditClick(event: any, index: number) {
-    this.editProject.projectID = this.projects[index].projectID;
-    this.editProject.projectName = this.projects[index].projectName;
-    this.editProject.dateOfStart = this.projects[index].dateOfStart;
-    this.editProject.teamSize = this.projects[index].teamSize;
-    this.editProject.active = this.projects[index].active;
-    this.editProject.clientLocationID = this.projects[index].clientLocationID;
-    this.editProject.clientLocation = this.projects[index].clientLocation;
-    this.editProject.status = this.projects[index].status;
-    this.editIndex = index;
-  }
-
-  onUpdateClick() {
-    this.projectsService.updateProject(this.editProject).subscribe(
-      (response: Project) => {
+    if (this.newForm.valid) {
+      this.newProject.clientLocation.clientLocationID = 0;
+      this.projectsService.insertProject(this.newProject).subscribe((response) => {
+        //Add Project to Grid
         var p: Project = new Project();
         p.projectID = response.projectID;
         p.projectName = response.projectName;
@@ -92,20 +63,69 @@ export class ProjectsComponent implements OnInit {
         p.active = response.active;
         p.clientLocationID = response.clientLocationID;
         p.status = response.status;
-        this.projects[this.editIndex] = p;
+        this.projects.push(p);
 
-        this.editProject.projectID = null;
-        this.editProject.projectName = null;
-        this.editProject.dateOfStart = null;
-        this.editProject.teamSize = null;
+        //Clear New Project Dialog - TextBoxes
+        this.newProject.projectID = null;
+        this.newProject.projectName = null;
+        this.newProject.dateOfStart = null;
+        this.newProject.teamSize = null;
         this.newProject.active = false;
         this.newProject.clientLocationID = null;
         this.newProject.status = null;
-      },
-      (error) => {
+
+        $('#newFormCancel').trigger('click');
+      }, (error) => {
         console.log(error);
-      }
-    );
+      });
+    }
+  }
+
+  onEditClick(event: any, index: number) {
+    this.editForm.resetForm();
+    setTimeout(() => {
+      this.editProject.projectID = this.projects[index].projectID;
+      this.editProject.projectName = this.projects[index].projectName;
+      this.editProject.dateOfStart = this.projects[index].dateOfStart;
+      this.editProject.teamSize = this.projects[index].teamSize;
+      this.editProject.active = this.projects[index].active;
+      this.editProject.clientLocationID = this.projects[index].clientLocationID;
+      this.editProject.clientLocation = this.projects[index].clientLocation;
+      this.editProject.status = this.projects[index].status;
+      this.editIndex = index;
+    }, 100);
+  }
+
+  onUpdateClick() {
+    if (this.editForm.valid) {
+      this.projectsService.updateProject(this.editProject).subscribe(
+        (response: Project) => {
+          var p: Project = new Project();
+          p.projectID = response.projectID;
+          p.projectName = response.projectName;
+          p.dateOfStart = response.dateOfStart;
+          p.teamSize = response.teamSize;
+          p.clientLocation = response.clientLocation;
+          p.active = response.active;
+          p.clientLocationID = response.clientLocationID;
+          p.status = response.status;
+          this.projects[this.editIndex] = p;
+
+          this.editProject.projectID = null;
+          this.editProject.projectName = null;
+          this.editProject.dateOfStart = null;
+          this.editProject.teamSize = null;
+          this.newProject.active = false;
+          this.newProject.clientLocationID = null;
+          this.newProject.status = null;
+
+          $('#editFormCancel').trigger('click');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 
   onDeleteClick(event: any, index: number) {
