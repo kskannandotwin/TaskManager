989c18fb5b6015723e4b45c18999b5148397fe70
kskannandotwin <51730866+kskannandotwin@users.noreply.github.com>
2023-01-05 15:44:58 +0530

    Update admin.module.ts

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,7 +9,6 @@ import { FormsModule } from '@angular/forms';
 import { TeamSizeValidatorDirective } from '../team-size-validator.directive';
 import { ClientLocationStatusValidatorDirective } from '../client-location-status-validator.directive';
 import { ProjectIDUniqueValidatorDirective } from '../project-idunique-validator.directive';
-import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
@@ -19,8 +18,7 @@ import { ProjectComponent } from './project/project.component';
     ProjectsComponent,
     TeamSizeValidatorDirective,
     ClientLocationStatusValidatorDirective,
-    ProjectIDUniqueValidatorDirective,
-    ProjectComponent
+    ProjectIDUniqueValidatorDirective
   ],
   imports: [
     CommonModule,
