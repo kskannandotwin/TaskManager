3242e6e37af7cc615fac33778d1042fd047b27d9
kannanwin <kskannan.win@gmail.com>
2023-01-09 09:39:25 +0530

    09.01.2023
    80. child to parent communication using output binding with custom events

--- src/app/admin/project/project.component.ts
+++ src/app/admin/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Project } from 'src/app/project';
 
 @Component({
@@ -10,11 +10,14 @@ export class ProjectComponent {
   @Input('currentProject') project: Project | any = null;
   @Input('recordIndex') i: number = 0;
 
-  onEditClick($event: any, i: number) {
+  @Output() editClick = new EventEmitter();
+  @Output() deleteClick = new EventEmitter();
 
+  onEditClick($event: any, i: any) {
+    this.editClick.emit({ $event, i });
   }
 
-  onDeleteClick($event: any, i: number) {
-
+  onDeleteClick($event: any, i: any) {
+    this.deleteClick.emit({ $event, i });
   }
 }
