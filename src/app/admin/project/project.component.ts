cffad483308c47b5fff1f146aa6d238dd6c15017
kannanwin <kskannan.win@gmail.com>
2023-01-18 16:00:28 +0530

    18.01.2023
    98. ngafterviewinit and ngafterviewcheked have completed

--- src/app/admin/project/project.component.ts
+++ src/app/admin/project/project.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, Component, ContentChild, ContentChildren, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, QueryList, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, ContentChildren, DoCheck, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, QueryList, SimpleChanges, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Project } from 'src/app/project';
 import { ProjectsService } from 'src/app/projects.service';
@@ -9,7 +9,7 @@ import { CheckBoxPrinterComponent } from '../check-box-printer/check-box-printer
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked {
+export class ProjectComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
   @Input('currentProject') project: Project | any = null;
   @Input('recordIndex') i: number = 0;
 
@@ -54,7 +54,18 @@ export class ProjectComponent implements OnChanges, OnInit, DoCheck, AfterConten
 
   ngAfterContentChecked() {
     console.info('.....ngAfterContentChecked called');
+    console.log(this.tbl);
   }
+
+  ngAfterViewInit() {
+    console.info('.....ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked(): void {
+    console.info('......ngAfterViewChecked called');
+  }
+
+  @ViewChild('tbl') tbl: ElementRef;
 
   onEditClick($event: any, i: any) {
     this.editClick.emit({ $event, i });
