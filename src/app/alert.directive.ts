044b3c8efb3e2d6fe73d3af27dc8c59c3f085ba0
kannanwin <kskannan.win@gmail.com>
2023-01-20 09:55:20 +0530

    20.01.2023
    108. receive parameters using input properties in directives has completed

--- src/app/alert.directive.ts
+++ src/app/alert.directive.ts
@@ -1,14 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAlert]'
 })
 export class AlertDirective {
 
-  constructor(private elementRef: ElementRef) {
+  @Input('error') error: string;
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
     this.elementRef.nativeElement.innerHTML = `
-    <div class="alert alert-danger fade show">
-      <span>Welcome</span>
+    <div class="alert alert-danger fade show" role="alert">
+      <span>${this.error}</span>
     </div>
     `;
   }
