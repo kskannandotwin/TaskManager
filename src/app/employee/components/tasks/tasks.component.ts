c2ca6d4b456c4cfa95f6c0a1ce58527615100cf8
kannanATexcelforte <kannan@excelforte.com>
2023-03-16 11:30:07 +0530

    16.03.2023
    147. apply contextual class to task has completed

--- src/app/employee/components/tasks/tasks.component.ts
+++ src/app/employee/components/tasks/tasks.component.ts
@@ -21,4 +21,41 @@ export class TasksComponent implements OnInit {
       this.taskGroups = response;
     });
   }
+
+  // get background color based on task status
+  getTaskGroupBgCssClass(taskStatusName: any): string {
+    let className: any;
+    switch (taskStatusName) {
+      case "Holding": className = "bg-secondary text-white"; break;
+      case "Prioritized": className = "bg-primary text-white"; break;
+      case "Started": className = "bg-info text-white"; break;
+      case "Finished": className = "bg-success text-white"; break;
+      case "Reverted": className = "bg-danger text-white"; break;
+    }
+    return className;
+  }
+
+  // get background color based on task priority
+  getTaskPriorityBadgeCssClass(taskPriorityName: any): string {
+    let className: any;
+    switch (taskPriorityName) {
+      case "Urgent": className = "badge-danger"; break;
+      case "Normal": className = "badge-primary"; break;
+      case "Below Normal": className = "badge-info"; break;
+      case "Low": className = "badge-secondary"; break;
+    }
+    return className;
+  }
+
+  // get text color based on task status
+  getTaskGroupTextCssClass(taskStatusName: any): string {
+    let className: any;
+    switch (taskStatusName) {
+      case "Holding": className = "text-secondary"; break;
+      case "Prioritized": className = "text-primary"; break;
+      case "Started": className = "text-info"; break;
+      case "Reverted": className = "text-red"; break;
+    }
+    return className;
+  }
 }
